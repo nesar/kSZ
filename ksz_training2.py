fe48c10ead391d473bcf192061f51f8cf3eb26f0
YWyuyu <physicsdonna@hotmail.com>
2018-09-09 20:40:08 -0500

    No normalization trainning

--- ksz_training2.py
+++ ksz_training2.py
@@ -23,8 +23,8 @@ K.set_image_dim_ordering('tf')
 
 batch_size = 2
 num_para = 1
-epochs = 10
-filenum = 16
+epochs = 50
+filenum = 1000
 test_split = 0.2
 num_para = 1
 image_size = 256 #1024
@@ -33,8 +33,7 @@ learning_rate = 0.0001
 decay_rate = 0.1
 
 
-input_dir = '../kSZ/Data/Box0/lowres/'
-save_dir = input_dir + 'Models'
+save_dir = '../saved_models/'
 model_name = 'keras_cifar10_trained_model.h5'
 
 # The data, split between train and test sets:
@@ -44,7 +43,7 @@ ksz_matrix = np.zeros((filenum, image_size, image_size))
 tsz_matrix = np.zeros((filenum, image_size, image_size))
 velocity_array = np.zeros((filenum))
 
-f1 = open(input_dir + "wmap.014.cluster.sim.dat")
+f1 = open("wmap.014.cluster.sim.dat")
 lines2 = f1.readlines()
 data = [line.split() for line in lines2]
 data = data[1:]
@@ -54,10 +53,10 @@ f1.close()
 
 for fi in range (filenum):
     # fileIn = "mymap_box0_s14_new_"+str(fi)+".014.a.z.fits"
-    fileIn = "mymap_box0_s14_is256_new_"+str(fi)+".014.a.z.fits"
+    fileIn = "Box0/mymap_box0_s14_is256_new_"+str(fi)+".014.a.z.fits"
 
 
-    f0=fits.open(input_dir + fileIn)
+    f0=fits.open(fileIn)
     ksz_matrix[fi] = f0[1].data
     f0.close()
 
@@ -77,6 +76,7 @@ np.random.seed(1234)
 shuffleOrder = np.arange(filenum)
 np.random.shuffle(shuffleOrder)
 ksz_matrix = ksz_matrix[shuffleOrder]
+ksz_matrix = np.arcsinh(1000000*ksz_matrix)
 #tsz_matrix = tsz_matrix[shuffleOrder]
 velocity_array = velocity_array[shuffleOrder]
 #allPara = np.dstack((ksz_matrix, tsz_matrix))[0]
@@ -94,27 +94,27 @@ y_test = velocity_array[num_train:filenum]
 # x_test = x_test.astype('float32')
 
 
-x_train -= np.min(ksz_matrix)
-x_test -= np.min(ksz_matrix)
-
-normFactor = np.max( [np.max(x_train), np.max(x_test ) ])
-
-
-x_train /= normFactor
-x_test /= normFactor
-
-x_train = np.expand_dims(x_train, axis=1)
-y_train = np.expand_dims(y_train, axis=1)
-
-
-
-y_train -= np.min(velocity_array)
-y_test -= np.min(velocity_array)
-
-y_normFactor = np.max( [np.max(y_train), np.max(y_test ) ])
-
-y_train /= y_normFactor
-y_test /= y_normFactor
+#x_train -= np.min(ksz_matrix)
+#x_test -= np.min(ksz_matrix)
+#
+#normFactor = np.max( [np.max(x_train), np.max(x_test ) ])
+#
+#
+#x_train /= normFactor
+#x_test /= normFactor
+#
+#x_train = np.expand_dims(x_train, axis=1)
+#y_train = np.expand_dims(y_train, axis=1)
+#
+#
+#
+#y_train -= np.min(velocity_array)
+#y_test -= np.min(velocity_array)
+#
+#y_normFactor = np.max( [np.max(y_train), np.max(y_test ) ])
+#
+#y_train /= y_normFactor
+#y_test /= y_normFactor
 
 # x_train = np.rollaxis(x_train, 2, 0)
 # x_train = np.rollaxis(x_train, 1, 0)
@@ -140,9 +140,9 @@ model.add(Conv2D(32, (3, 3),
 model.add(Activation('relu'))
 
 
-model.add(Conv2D(32, (3, 3)))
+model.add(Conv2D(32, (3, 3)))                               #32 is number of kernell, size of the kernell
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))                   #256*256 to 128*128
 # model.add(Dropout(0.25))
 
 model.add(Conv2D(32, (3, 3), padding='same'))
@@ -168,7 +168,15 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 # #
 
 model.add(Flatten())
+model.add(Dense(10000))
+model.add(Activation('relu'))
 model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(32))
+model.add(Activation('relu'))
+model.add(Dense(8))
 model.add(Activation('relu'))
 # model.add(Dropout(0.5))
 model.add(Dense(num_para))
